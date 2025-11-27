f26828d68dd8251a9250f0d3cce1d54d8c89fc1c
sidhantakumar7077 <69065858+sidhantakumar7077@users.noreply.github.com>
2025-11-27 17:25:48 +0530

    SafeAreaProvider + SafeAreaView in index.js

--- index.js
+++ index.js
@@ -2,11 +2,25 @@
  * @format
  */
 
+import React from 'react';
+import { AppRegistry } from 'react-native';
 import TrackPlayer from 'react-native-track-player';
-import {AppRegistry} from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
 import playerService from './src/Component/Service';
 
-AppRegistry.registerComponent(appName, () => App);
+const Root = () => (
+    <SafeAreaProvider>
+        <SafeAreaView
+            style={{ flex: 1, backgroundColor: '#341551' }} // change bg if you want
+            edges={['top', 'bottom']}                       // protect top & bottom
+        >
+            <App />
+        </SafeAreaView>
+    </SafeAreaProvider>
+);
+
+AppRegistry.registerComponent(appName, () => Root);
 TrackPlayer.registerPlaybackService(() => playerService);
